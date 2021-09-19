77e4760b251976572e874f0a3a85cf54547cb542
Jiacheng787 <xiaosuanjia@126.com>
2021-09-19 22:29:32 +0800

    :wrench: chore: 生产环境对 JS 和 CSS 进行压缩

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const webpack = require('webpack');
 const { merge } = require("webpack-merge");
 const baseConfig = require("./webpack.base.config");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const envParams = require('dotenv').config({ path: path.resolve(__dirname, '../.env.production') });
 
 module.exports = merge(baseConfig, {
@@ -9,5 +11,19 @@ module.exports = merge(baseConfig, {
   stats: "errors-only",
   plugins: [
     new webpack.EnvironmentPlugin(Object.keys(envParams.parsed))
-  ]
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+            drop_debugger: true,
+          },
+        }
+      }),
+      new CssMinimizerPlugin(), // 生产环境压缩 CSS
+    ]
+  }
 })
